216225fd7156b2e3c5f3513ad084704ef9478b2c
thechampsant <thechampsant@gmail.com>
2022-08-23 13:17:45 +0530

    Added credentials to Fetch

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000
-
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
